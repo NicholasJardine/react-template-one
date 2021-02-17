1704b4e2ac275b7dc235a1a6ee670c399972540d
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-02-17 12:28:00 +0200

    service section is structured

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,9 @@ import Navbar from '../components/navbar';
 import HeroSection from '../components/HeroSection'
 import InfoSection from '../components/infoSection';
 import { homeObjOne, homeObjTwo, homeObjThree } from '../components/infoSection/data';
+import Services from '../components/Services';
+
+
 const Home = () => {
 
     const [isOpen, setIsOpen] = useState(false)
@@ -20,6 +23,7 @@ const Home = () => {
         <HeroSection></HeroSection>
         <InfoSection {...homeObjOne} />
         <InfoSection {...homeObjTwo} />
+        <Services></Services>
         <InfoSection {...homeObjThree} />
 
         </>
